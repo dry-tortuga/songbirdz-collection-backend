48c78466a54a4f1d25735f9cacf80368bfa7ba98
dry-tortuga <108436943+dry-tortuga@users.noreply.github.com>
2024-04-18 17:06:20 -0400

    make images public

--- server/routes/index.js
+++ server/routes/index.js
@@ -170,8 +170,10 @@ router.get("/metadata/:id", async (req, res, next) => {
 		name: `Songbird #${birdId}`,
 		description,
 		external_url: `${process.env.SONGBIRDZ_BACKEND_URL}/collection/${birdId}`,
-		image: `${process.env.SONGBIRDZ_BACKEND_URL}/birds/image-lg/${birdId}`,
-		image_onchain: `${process.env.SONGBIRDZ_BACKEND_URL}/birds/image/${birdId}`,
+		// image: `${process.env.SONGBIRDZ_BACKEND_URL}/birds/image-lg/${birdId}`,
+		// image_onchain: `${process.env.SONGBIRDZ_BACKEND_URL}/birds/image/${birdId}`,
+		image: `${process.env.SONGBIRDZ_BACKEND_URL}/images/${birdId}-lg.jpg`,
+		image_onchain: `${process.env.SONGBIRDZ_BACKEND_URL}/images/${birdId}.jpg`,
 		audio_onchain: `${process.env.SONGBIRDZ_BACKEND_URL}/audio/${birdId}.mp3`,
 		species,
 		attributes: [{
