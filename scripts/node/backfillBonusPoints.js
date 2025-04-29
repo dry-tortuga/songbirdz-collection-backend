72e0886a812fb7fc40680b4b4ed23ed37f8a9807
dry-tortuga <108436943+dry-tortuga@users.noreply.github.com>
2025-04-29 09:52:47 -0400

    add jbpunke to twitter posts

--- scripts/node/backfillBonusPoints.js
+++ scripts/node/backfillBonusPoints.js
@@ -12,6 +12,7 @@ const SEASON_4_BONUS_POINT_SPECIES = [{
 	users: [
 		'0x79e47f12fe6382cfb29e8e7dc05c57b5c04cc814', // paesan
 		'0xaaa87024aa2fd6e816161cc2554f47a5256bb57d', // GlenCanete
+		'0x4d389ddfa4984735b6988b2d853522925ee8ec6b', // jbpunke
 	],
 }, {
 	id: 1014,
