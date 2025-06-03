9d56d7babbdce19e4d4aec93239cd65b74e8e68b
dry-tortuga <108436943+dry-tortuga@users.noreply.github.com>
2025-06-03 12:07:47 -0400

    add dbin to farcaster channel

--- scripts/node/backfillBonusPoints.js
+++ scripts/node/backfillBonusPoints.js
@@ -86,6 +86,7 @@ const SEASON_4_BONUS_POINT_SPECIES = [{
 		'0x11415669286d998fd85a2a6146c41f6be41a794d', // reisub.base.eth
 		'0x9beadc1297f97fff916a487e49dea1004510c59e', // s0tric.base.eth
 		'0x8d888326e1e2b6a399b751105339f82476208d5d', // tomkey
+		'0x2fe61e1fcb4fd2453ed34ca57a648db4d71a9232', // dbin78
 	],
 }, {
 	id: 1017,
