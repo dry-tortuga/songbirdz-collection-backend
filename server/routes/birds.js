22702c1a377d662c6da627ca94022d1fe66a5658
dry-tortuga <108436943+dry-tortuga@users.noreply.github.com>
2025-10-10 14:38:30 -0400

    finalize audio author

--- server/routes/birds.js
+++ server/routes/birds.js
@@ -58,12 +58,15 @@ const getBirdMetadata = async (req, res, next) => {
 	const description = isIdentified ? null : 'This bird has not been identified yet.';
 	const family = SOURCE_SPECIES_DATA[KEY_BIRD_DATA[birdId].name]?.family;
 
+	const audioSpecies = KEY_BIRD_DATA[birdId]?.audioSpecies;
+	const audioContributor = KEY_BIRD_DATA[birdId]?.audioContributor;
+
 	// See below the required JSON structure for metadata
 	// https://docs.opensea.io/docs/getting-started
 
 	res.send({
 		name: `Songbird #${birdId}`,
-		description: 'One of the 10,000 birds in the Songbirdz collection',
+		description: `One of the 10,000 birds in the Songbirdz collection. Curated by drytortuga. Image: DALL-E 3. Audio: ${audioSpecies} © ${audioContributor}; Cornell Lab of Ornithology | Macaulay Library.`,
 		animation_url: `${process.env.SONGBIRDZ_FRONTEND_URL}/audio/${birdId}.mp3`,
 		external_url: `${process.env.SONGBIRDZ_FRONTEND_URL}/collection/${birdId}`,
 		image: `${process.env.SONGBIRDZ_FRONTEND_URL}/images/${birdId}-lg.jpg`,
